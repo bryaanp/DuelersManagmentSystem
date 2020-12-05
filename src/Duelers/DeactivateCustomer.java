6425de40796db6f06c3ac0178e57c430c8f70972
Bryan Nguyen <bryan.nguyen12349@gmail.com>
2020-12-05 03:23:50 -0800

    Recreate DeactivateCustomer

--- src/Duelers/DeactivateCustomer.java
+++ src/Duelers/DeactivateCustomer.java
@@ -7,27 +7,12 @@ package Duelers;
 
 /**
  *
- * @author Bryan
+ * @author majes
  */
-import java.sql.Driver;
-import java.awt.HeadlessException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.*;
-import javax.swing.*;
-import javax.swing.JOptionPane;
-import java.awt.Toolkit;
-import java.awt.event.WindowEvent;
-import static java.lang.ProcessBuilder.Redirect.from;
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
 public class DeactivateCustomer extends javax.swing.JPanel {
 
     /**
-     * Creates new form RemoveCustomer
+     * Creates new form DeactivateCustomer
      */
     public DeactivateCustomer() {
         initComponents();
@@ -42,101 +27,19 @@ public class DeactivateCustomer extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnSubmit = new javax.swing.JButton();
-        btnReturn = new javax.swing.JButton();
-        lbDeactivateCustomer = new javax.swing.JLabel();
-        lbSelectCustomer = new javax.swing.JLabel();
-        combSelectCustomer = new javax.swing.JComboBox<>();
-
-        btnSubmit.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        btnSubmit.setText("Submit");
-        btnSubmit.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSubmitActionPerformed(evt);
-            }
-        });
-
-        btnReturn.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        btnReturn.setText("Return");
-        btnReturn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnReturnActionPerformed(evt);
-            }
-        });
-
-        lbDeactivateCustomer.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        lbDeactivateCustomer.setText("Deactivate Customer");
-
-        lbSelectCustomer.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        lbSelectCustomer.setText("Select Customer");
-
-        combSelectCustomer.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(lbDeactivateCustomer)
-                .addGap(96, 96, 96))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(btnSubmit, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
-                .addComponent(btnReturn, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(37, 37, 37))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(lbSelectCustomer)
-                .addGap(18, 18, 18)
-                .addComponent(combSelectCustomer, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(41, 41, 41))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lbDeactivateCustomer)
-                .addGap(54, 54, 54)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbSelectCustomer)
-                    .addComponent(combSelectCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 120, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnSubmit)
-                    .addComponent(btnReturn))
-                .addGap(23, 23, 23))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
-        Connection conn;
-        MyConnection submitDeactivateCust = new MyConnection();
-        conn = submitDeactivateCust.getConnection();
-        //String sql= "insert into warehouse (warehouseNumber, warehouseAddress)" + "values(?,?)";
-        //String sql = 
-
-        try {
-
-            PreparedStatement pst= conn.prepareStatement(sql);
-            // pst.setString(1, txtNumber.getText());
-            // pst.setString(2, txtAddress.getText());
-            pst.executeUpdate(sql);
-
-    }//GEN-LAST:event_btnSubmitActionPerformed
-
-    private void btnReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReturnActionPerformed
-        Customer Info = new Customer();
-        Info.setVisible(true);
-    }//GEN-LAST:event_btnReturnActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnReturn;
-    private javax.swing.JButton btnSubmit;
-    private javax.swing.JComboBox<String> combSelectCustomer;
-    private javax.swing.JLabel lbDeactivateCustomer;
-    private javax.swing.JLabel lbSelectCustomer;
     // End of variables declaration//GEN-END:variables
 }
