a60ce44d879adddba9b4cdd84b4e949cb8bb87a0
Bryan Nguyen <bryan.nguyen12349@gmail.com>
2020-12-06 18:35:15 -0800

    Addwarehouse

--- src/Duelers/AddWarehouse.java
+++ src/Duelers/AddWarehouse.java
@@ -4,23 +4,16 @@
  * and open the template in the editor.
  */
 package Duelers;
-import java.sql.Driver;
-import java.awt.HeadlessException;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.*;
-import javax.swing.*;
 import javax.swing.JOptionPane;
-import java.awt.Toolkit;
-import java.awt.event.WindowEvent;
-import static java.lang.ProcessBuilder.Redirect.from;
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
 
-
+/**
+ *
+ * @author majes
+ */
 public class AddWarehouse extends javax.swing.JFrame {
 
     /**
@@ -39,145 +32,131 @@ public class AddWarehouse extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         lbAddWarehouse = new javax.swing.JLabel();
-        lbNumber = new javax.swing.JLabel();
-        txtNumber = new javax.swing.JTextField();
-        lbAddress = new javax.swing.JLabel();
+        lbWarehouseNum = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        btnCancel = new javax.swing.JButton();
+        btnSubmit = new javax.swing.JButton();
+        txtWarehouseNum = new javax.swing.JTextField();
         txtAddress = new javax.swing.JTextField();
-        btnReturn = new javax.swing.JButton();
-        jSeparator1 = new javax.swing.JSeparator();
-        btnSubmit1 = new javax.swing.JButton();
+        lbWarehouseStock = new javax.swing.JLabel();
+        txtWarehouseStock = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         lbAddWarehouse.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         lbAddWarehouse.setText("Add Warehouse");
 
-        lbNumber.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        lbNumber.setText("Number");
+        lbWarehouseNum.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
+        lbWarehouseNum.setText("Warehouse Number");
 
-        txtNumber.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
+        jLabel1.setText("Address");
 
-        lbAddress.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        lbAddress.setText("Address");
-
-        txtAddress.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        btnReturn.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        btnReturn.setText("Return");
-        btnReturn.addActionListener(new java.awt.event.ActionListener() {
+        btnCancel.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
+        btnCancel.setText("Cancel");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnReturnActionPerformed(evt);
+                btnCancelActionPerformed(evt);
             }
         });
 
-        btnSubmit1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        btnSubmit1.setText("Submit");
-        btnSubmit1.addActionListener(new java.awt.event.ActionListener() {
+        btnSubmit.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
+        btnSubmit.setText("Submit");
+        btnSubmit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSubmit1ActionPerformed(evt);
+                btnSubmitActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jSeparator1)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(94, 94, 94)
-                        .addComponent(lbAddWarehouse, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(lbNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(txtNumber))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(lbAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(txtAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(55, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(btnReturn, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(68, 68, 68))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(59, 59, 59)
-                    .addComponent(btnSubmit1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(223, Short.MAX_VALUE)))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(lbAddWarehouse)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbNumber)
-                    .addComponent(txtNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbAddress)
-                    .addComponent(txtAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 75, Short.MAX_VALUE)
-                .addComponent(btnReturn)
-                .addGap(19, 19, 19))
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                    .addContainerGap(251, Short.MAX_VALUE)
-                    .addComponent(btnSubmit1)
-                    .addGap(18, 18, 18)))
-        );
+        lbWarehouseStock.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
+        lbWarehouseStock.setText("Warehouse Stock");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lbWarehouseNum)
+                            .addComponent(jLabel1)
+                            .addComponent(lbWarehouseStock))
+                        .addGap(26, 26, 26)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtWarehouseNum)
+                            .addComponent(txtAddress)
+                            .addComponent(txtWarehouseStock, javax.swing.GroupLayout.DEFAULT_SIZE, 211, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(86, 86, 86)
+                        .addComponent(btnCancel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 99, Short.MAX_VALUE)
+                        .addComponent(btnSubmit)
+                        .addGap(51, 51, 51)))
+                .addContainerGap(27, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(lbAddWarehouse)
+                .addGap(119, 119, 119))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lbAddWarehouse)
+                .addGap(57, 57, 57)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbWarehouseNum)
+                    .addComponent(txtWarehouseNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(txtAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbWarehouseStock)
+                    .addComponent(txtWarehouseStock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCancel)
+                    .addComponent(btnSubmit))
+                .addGap(26, 26, 26))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReturnActionPerformed
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+        // TODO add your handling code here:
         dispose();
         new Warehouse().setVisible(true);
-    }//GEN-LAST:event_btnReturnActionPerformed
+    }//GEN-LAST:event_btnCancelActionPerformed
 
-    private void btnSubmit1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmit1ActionPerformed
+    private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
+        // TODO add your handling code here:
         Connection conn;
-       MyConnection submitwarehouse = new MyConnection();
-       conn = submitwarehouse.getConnection();
-       //String sql= "insert into warehouse (warehouseNumber, warehouseAddress)" + "values(?,?)";
-       String sql = "insert into warehouse (warehouseNumber, warehouseAddress) values ( '" + txtNumber.getText() + "' , '" + txtAddress.getText() + "')"; 
+        MyConnection addWarehouse = new MyConnection();
+        conn = addWarehouse.getConnection();
+        String sql = "insert into customer (warehouseNumber, warehouseAddress, warehouseStock) "
+        + "values ( '" + txtWarehouseNum.getText() + "' , '" + txtAddress.getText() + "' , '" +
+        txtWarehouseStock.getText() +"')";
 
-    try {
-        PreparedStatement pst= conn.prepareStatement(sql);
-        pst.executeUpdate(sql);
-        JOptionPane.showMessageDialog(null, "Warehouse " + txtNumber.getText() + " has been added.");
-        
-    }//GEN-LAST:event_btnSubmit1ActionPerformed
-    catch (SQLException ex)
-    {
-        JOptionPane.showMessageDialog(null, ex);
-    }  
-    
- }
-    
+        try {
+
+            PreparedStatement pst= conn.prepareStatement(sql);
+            pst.executeUpdate(sql);
+
+        }
+        catch (SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+        dispose();
+        new Customer().setVisible(true);
+    }//GEN-LAST:event_btnSubmitActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -214,14 +193,14 @@ public class AddWarehouse extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnReturn;
-    private javax.swing.JButton btnSubmit1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnSubmit;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel lbAddWarehouse;
-    private javax.swing.JLabel lbAddress;
-    private javax.swing.JLabel lbNumber;
+    private javax.swing.JLabel lbWarehouseNum;
+    private javax.swing.JLabel lbWarehouseStock;
     private javax.swing.JTextField txtAddress;
-    private javax.swing.JTextField txtNumber;
+    private javax.swing.JTextField txtWarehouseNum;
+    private javax.swing.JTextField txtWarehouseStock;
     // End of variables declaration//GEN-END:variables
 }
